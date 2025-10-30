8473c586ebbf51c68e93f98182bf2a0493048c6a
pramode-pandit <83385177+pramode-pandit@users.noreply.github.com>
2025-10-30 22:59:24 +0530

    Add files via upload

--- 01-backend-bootstrap/main.tf
+++ 01-backend-bootstrap/main.tf
@@ -1,1 +1,46 @@
-www
+# -----------------------------
+# S3 Bucket for Terraform State
+# -----------------------------
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+# Block public access
+resource "aws_s3_bucket_public_access_block" "tf_state_block" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable versioning to preserve history of state files
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# -----------------------------
+# DynamoDB Table for State Locking
+# -----------------------------
+resource "aws_dynamodb_table" "tf_lock_table" {
+  name           = var.dynamodb_table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock Table"
+  }
+}
---
+++ 01-backend-bootstrap/variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {
+  default = "bootstrap-tf-sol9092-bucket"
+}
+
+variable "dynamodb_table_name" {
+  default = "terraform-locks"
+}
